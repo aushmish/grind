c44c1477ce12dd112a6fc71971b283f5e3466503
Anushka Mishra <nicepersonanushka@gmail.com>
2025-04-24 00:28:11 +0530

    Add files via upload

---
+++ settimeout.js
@@ -0,0 +1,28 @@
+// console.log("hi");
+
+// function timeout(){
+//     console.log("hey there wp!");
+// }
+
+// setTimeout(timeout,5000);
+
+// console.log("Hello There gm!");
+
+
+
+function timeout(){
+    console.log("Hello!");
+}
+
+console.log("hi");
+
+setTimeout(timeout,500);
+
+console.log("Welcome to loop!!")
+
+c=0;
+for(i=0;i<10000000000;i++){
+    c+=1;
+}
+
+console.log("Expensive Operation Done !");
