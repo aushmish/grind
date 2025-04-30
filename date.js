a431e04bdeba57ee962cdc872f30fdbd696401f2
Anushka Mishra <nicepersonanushka@gmail.com>
2025-04-30 12:08:23 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,18 @@
+// const date=new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.getDay());
+// console.log(date.toISOString());
+
+
+const map=new Map();
+map.set('Name','Anushka');
+map.set('Class',12);
+console.log(map.get('Name'));
+
+
+console.log(map['Name']);
+
+
+const name ="Anushka";
+console.log(name);
